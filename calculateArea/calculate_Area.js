4d35f5ffcc2025bffde3cd52e91be513f4b40614
segmur <shegun524@gmail.com>
2025-06-06 14:09:40 -0400

    update calculate_Area.html and calculate_Area.js

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,12 @@ function calculateArea() {
 let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker() {
+    groceryAmount1 = parseFloat(document.getElementById('grocery1').value);
+    groceryAmount2 = parseFloat(document.getElementById('grocery2').value);
+    groceryAmount3 = parseFloat(document.getElementById('grocery3').value);
+
+   let totalAmount = groceryAmount1 + groceryAmount2 + groceryAmount3;
+    document.getElementById('groceryAmount').innerText = `The total amount is: ${totalAmount}`;
+   }
